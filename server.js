eb6c198c069598e793d61713f89a4018eac24e75
81khameh <khazar.mehraban@gmail.com>
2020-06-16 14:57:07 +0200

    see last commit

--- server.js
+++ server.js
@@ -9,11 +9,22 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3002;
 
 const app = express();
+const db=require('./queries');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("./app/routes/customer.routes")(app);
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+app.get('/users',db.getUsers);
+app.get('/users/:id',db.getUserById);
+app.post('/users',db.createUser);
+app.put('/users/:id',db.updateUser);
+app.delete('/users/:id',db.deleteUser);
+
+
+//require("./app/routes/customer.routes")(app);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
