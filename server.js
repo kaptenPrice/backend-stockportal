90832f407a83c720651fc87f5727acc8e3b2540d
81khameh <khazar.mehraban@gmail.com>
2020-06-18 11:29:40 +0200

    fixing the bransch

--- server.js
+++ server.js
@@ -17,17 +17,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
-// app.get('/users',userdb.getUsers);
-// app.get('/users/:id',userdb.getUserById);
-// app.post('/newuser',userdb.createUser);
-// app.put('/users/:id',userdb.updateUser);
-// app.put('/userpass/:id',userdb.updatePassword);
-// app.delete('/users/:id',userdb.deleteUser);
-// app.post('/preference',userdb.addPreference);
-// app.delete('/preference', userdb.removePreference);
-// app.get('/preference', userdb.getPreferences);
-// app.get('/portfolio',portfoliodb.getPortfolio);
-// app.get("")
+app.get('/users',userdb.getUsers);
+app.get('/users/:id',userdb.getUserById);
+app.post('/newuser',userdb.createUser);
+app.put('/users/:id',userdb.updateUser);
+app.put('/userpass/:id',userdb.updatePassword);
+app.delete('/users/:id',userdb.deleteUser);
+app.post('/preference',userdb.addPreference);
+app.delete('/preference', userdb.removePreference);
+app.get('/preference', userdb.getPreferences);
+app.get('/portfolio',portfoliodb.getPortfolio);
 
 //require("./app/routes/customer.routes")(app);
 
