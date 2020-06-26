edccea245f1c0f522c56bc8144a33391e60b313a
EpicCoffee <ludvig.ojl@gmail.com>
2020-06-26 23:52:26 +0200

    typo

--- portfolio-queries.js
+++ portfolio-queries.js
@@ -3,7 +3,7 @@ const {deCodeIdToken} = require("./app/utility");
 
 const getPortfolio = (req, response) => {
   const userid = deCodeIdToken(req.body.id_token);
-  sql.query(`SELECT portfolio.stockvalue, portfolio.stocktype, portfolio.amount, portfolio.stocknumber, portfolio.ownamount, portfolio.votevalue, portfolio.datepurchased, companies.companyname, categories.catname, categoris.color FROM public.portfolio JOIN public.companies ON portfolio.companyid=companies.companyid JOIN categories ON companies.catid=categories.catid WHERE userid = '${userid}'`, (error, results) => {
+  sql.query(`SELECT portfolio.stockvalue, portfolio.stocktype, portfolio.amount, portfolio.stocknumber, portfolio.ownamount, portfolio.votevalue, portfolio.datepurchased, companies.companyname, categories.catname, categories.color FROM public.portfolio JOIN public.companies ON portfolio.companyid=companies.companyid JOIN categories ON companies.catid=categories.catid WHERE userid = '${userid}'`, (error, results) => {
     if (error) {
       console.log(error)
       response.status(500).json({message : "Something went wrong when getting portfolio"}); 
