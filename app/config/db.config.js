e603cb86af98e63026a1f4900468ce9d79f20af4
isabelleromhagen <isabelle.romhagen@gmail.com>
2020-06-27 12:40:31 +0200

    now connected to db through elephantsql

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,9 +2,9 @@
 // I configen mappen kan andra hårdkodade värden också finnas.
 
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "1234",
-  DB: "stockportal",
+  HOST: "ruby.db.elephantsql.com",
+  USER: "ioshdihx",
+  PASSWORD: "h7qf9xRPJZY5vnPomh8GpIhU-8ejcX8z",
+  DB: "ioshdihx",
   PORT:"5432",
 };
